df31b1358354e1fb96976c6ad4d77947b2c5d892
Dimion <ardgalen@dimlev.ru>
2023-06-09 16:10:22 +0300

    initial and filtred arrays initialized

--- Program.cs
+++ Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿string[] array = new string[] { "alfa", "omega3", "yes", "no" }; // Создиние изначального массива
+System.Console.Write("Изначальный массив: "+"[" + string.Join(" ", array) + "]"); // Вывод изначального массива
+System.Console.WriteLine(); 
+string[] filteredArray = new string[array.Length]; // Создание нового массива для отфильтрованных элементов перед циклом
+
+int filteredIndex = 0; // Индекс для добавления элементов в filteredArray

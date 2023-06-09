4d921e64ba44054a3d2dc745c4e15a5dc668b4e5
Dimion <ardgalen@dimlev.ru>
2023-06-09 16:11:26 +0300

    for, and if\else added

--- Program.cs
+++ Program.cs
@@ -4,3 +4,12 @@ System.Console.WriteLine();
 string[] filteredArray = new string[array.Length]; // Создание нового массива для отфильтрованных элементов перед циклом
 
 int filteredIndex = 0; // Индекс для добавления элементов в filteredArray
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        filteredArray[filteredIndex] = array[i];
+        filteredIndex++; 
+    }
+}
